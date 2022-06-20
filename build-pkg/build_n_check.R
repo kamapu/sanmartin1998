c0d0067a17884778f6aaffd552c033087eeb2296
Miguel Alvarez <kamapu78@gmail.com>
2022-06-20 11:53:06 +0200

    Initial commit

---
+++ build-pkg/build_n_check.R
@@ -0,0 +1,30 @@
+# TODO:   Working script for testing the package 'taxlist'
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+library(devtools)
+library(styler)
+
+# Clean session
+rm(list = ls())
+
+# Clean folder
+unlink(file.path("build-pkg", list.files("build-pkg", ".tar.gz")))
+unlink(file.path("build-pkg", list.files("build-pkg", ".pdf")))
+
+# Write data
+## source("data-raw/create-data.R")
+
+# re-style scripts
+style_pkg()
+
+# write documentation
+document()
+
+# Build and check package
+pkg_loc <- build(path = "build-pkg", args = "--resave-data")
+check_built(path = pkg_loc)
+
+# write manual
+build_manual(path = "build-pkg")
